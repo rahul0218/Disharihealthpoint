30c3db26ff09392e2e9515f9e7b8320780929f02
Rahul kumar <kumar.rahul0218@gmail.com>
2021-07-06 15:04:53 +0530

    routing without nav

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,6 +1,12 @@
 <?php
     class Pages extends CI_Controller{
-        public function view($pages = 'home'){
-           if(!file_exists(APPPATH.'view')) 
+        public function view($page = 'home'){
+           if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
+               show_404();
+           }
+           $data['title'] = ucfirst($page);
+           $this->load->view('templates/header');
+           $this->load->view('pages/'.$page, $data);
+           $this->load->view('templates/footer');
         }
     }
